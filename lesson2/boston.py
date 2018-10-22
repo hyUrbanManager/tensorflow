cb678009fcd178f2aea7eb786188e8157ffaee89
hyUrbanManager <936947038@qq.com>
2018-10-22 23:37:20 +0800

    使用sklearn线性模型预测波士顿房价。

--- lesson2/boston.py
+++ lesson2/boston.py
@@ -7,20 +7,41 @@ import keras
 import sklearn 
 from sklearn.datasets import load_boston
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.optimizers import SGD
+
+from sklearn import linear_model
 
 # gain data.
 dir(load_boston())
 boston = load_boston()
 x = boston.data
 y = boston.target[:,np.newaxis]
-print(boston.feature_names)
-plt.scatter(x, y, label='ofo', c='b')
-plt.show()
+# train
+x_train = x[:450]
+y_train = y[:450]
+# pred
+x_test = x[450:]
+y_test = y[450:]
 
-# create mode.which model
+# 卷积网络模型。
 model = Sequential()
+model.add(Dense(units=64, input_dim=13))
+model.add(Activation("relu"))
+model.add(Dense(units=1))
+model.add(Activation("softmax"))
 model.compile(optimizer=SGD(lr=0.01),loss='mse',metrics=['accuracy'])
+# model.fit(x_train, y_train, epochs=6, batch_size=32)
+# pred = model.predict(x_test, batch_size=128)
 
-print(pred)
+# sklearn默认的线性模型。
+lr = linear_model.LinearRegression()
+lr.fit(x_train, y_train)
+pred = lr.predict(x_test)
+
+plt.scatter(pred, y_test, label='boston', c='b')
+plt.xlabel('pred')
+plt.ylabel('truth')
+plt.grid()
+plt.title('xld 15210320411')
+plt.show()
